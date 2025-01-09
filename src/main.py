baf9352cb30759081bb8a66682157a5047bc3682
Chanhong KIm <andrea9292@gmail.com>
2025-01-10 00:42:46 +0900

    Removal of unnecessary files.

--- src/main.py
+++
@@ -1,7 +0,0 @@
-def main():
-    print("Welcome to Pulldown Food Court!")
-
-
-if __name__ == "__main__":
-    main()
-    
--- src/pyside6_test.py
+++
@@ -1,8 +0,0 @@
-from PySide6.QtWidgets import QApplication, QMainWindow
-
-app = QApplication([])
-window = QMainWindow()
-window.setWindowTitle("PySide6 Installation Test")
-window.setGeometry(100, 100, 800, 600)
-window.show()
-app.exec()
